40d68564a434a4f601614686715eb161ccb95d65
Sachi <sachithra@efuturesworld.com>
2024-08-01 01:42:50 +0530

    response done

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,56 +1,19 @@
 import React, { useState } from 'react';
 import Tether from 'tether';
 import '../assets/style.css';
-import '../assets/script.js';  
-import axios from 'axios';
+import '../assets/script.js';
 
-// Attach Tether to the window object
 window.Tether = Tether;
 
 function Home() {
-    const [question, setQuestion] = useState("");
-
-    // Async function to generate answer
-    async function generateAnswer(event) {
-      console.log("loading");
-        
-
-        try {
-            const response = await axios({
-                url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAHourGI4um3wLcLun1pPuHfY5oSMeu4pQ",
-                method: "post",
-                data: {
-                    contents: [
-                        { parts: [{ text:  question}] },
-                    ],
-                },
-            });
-
-            console.log(
-                response['data']['candidates'][0]['content']['parts'][0]['text']
-            
-            
-            ); 
-
-
-        } catch (error) {
-            console.error('Error generating answer:', error);
-        }
-    }
-
     return (
         <div>
             <div id="center-text"></div>
             <div id="body">
-
-                {/* message icon on web page */}
-
                 <div id="chat-circle" className="btn btn-raised">
                     <div id="chat-overlay" />
                     <i className="material-icons">message</i>
                 </div>
-
-
 
                 <div className="chat-box">
                     <div className="chat-box-header">
@@ -59,18 +22,14 @@ function Home() {
                     </div>
                     <div className="chat-box-body">
                         <div className="chat-box-overlay"></div>
-                        <div className="chat-logs"></div> {/* chat-log */}
+                        <div className="chat-logs"></div>
                     </div>
 
                     <div className="chat-input">
-                      
-                        
-                            
-                            <textarea value={question} onChange={(e)=>setQuestion(e.target.value)} type="text" id="chat-input" placeholder="Send a message..." />
-                            <button onClick={generateAnswer} type="submit" className="chat-submit" id="chat-submit">
-                                <i className="material-icons">send</i>
-                            </button>
-                      
+                        <input type="text" id="chat-input" placeholder="Send a message..." />
+                        <button type="submit" className="chat-submit" id="chat-submit">
+                            <i className="material-icons">send</i>
+                        </button>
                     </div>
                 </div>
             </div>
