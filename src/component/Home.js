273f0203ac1052cf69d238a9b9b676760f30b8a9
Sachi <sachithra@efuturesworld.com>
2024-08-01 14:41:53 +0530

    final

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,28 +8,59 @@ window.Tether = Tether;
 function Home() {
     return (
         <div>
-            <div id="center-text"></div>
-            <div id="body">
-                <div id="chat-circle" className="btn btn-raised">
-                    <div id="chat-overlay" />
-                    <i className="material-icons">message</i>
+            <h1>Hi, I'm the AI Chatbot</h1>
+            <div class="outer-wrap">
+                <div class="wrap">
+                    <div class="bot">
+                        <div class="bot-wrap">
+                            <div class="antenna">
+                                <div class="antenna-base"></div>
+                                <div class="antenna-hook-left"></div>
+                                <div class="antenna-hook-right"></div>
+                            </div>
+                            <div class="head">
+                                <div class="eyes-wrap">
+                                    <div class="eyes"></div>
+                                    <div class="eyes"></div>
+                                </div>
+                            </div>
+                            <div class="body"></div>
+                            <div class="bottom"></div>
+                        </div>
+                        <div class="shadow"></div>
+                    </div>
+                    <div class="text"></div>
                 </div>
+            </div>
 
-                <div className="chat-box">
-                    <div className="chat-box-header">
-                        ChatBot
-                        <span className="chat-box-toggle"><i className="material-icons">close</i></span>
-                    </div>
-                    <div className="chat-box-body">
-                        <div className="chat-box-overlay"></div>
-                        <div className="chat-logs"></div>
+
+            <div>
+                <div id="center-text"></div>
+                <div id="body">
+                    <div id="chat-circle" class="btn btn-raised">
+                        <div id="chat-overlay"></div>
+                        <i class="material-icons">smart_toy</i>
                     </div>
 
-                    <div className="chat-input">
-                        <input type="text" id="chat-input" placeholder="Send a message..." />
-                        <button type="submit" className="chat-submit" id="chat-submit">
-                            <i className="material-icons">send</i>
-                        </button>
+                    <div class="chat-box">
+                        <div class="chat-box-header">
+                            <div class="online-status">
+                                <div class="online-dot"></div>
+                                Chatbot
+                            </div>
+                            <span class="chat-box-toggle"><i class="material-icons">close</i></span>
+                        </div>
+                        <div class="chat-box-body">
+                            <div class="chat-box-overlay"></div>
+                            <div class="chat-logs"></div>
+                        </div>
+
+                        <div class="chat-input">
+                            <input type="text" id="chat-input" placeholder="Send a message..." />
+                            <button type="submit" class="chat-submit" id="chat-submit">
+                                <i class="material-icons">send</i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
